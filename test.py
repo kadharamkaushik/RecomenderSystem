784e90f459740a94e1b7dbd2a58e150647427a0a
kadharamkaushik <kadharamkaushik@gmail.com>
2018-05-18 10:54:36 +0530

    date 18 May

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 import collections
 import math
-import operator
-
 import pandas as pd
 from pandas import DataFrame
-# import collections
 
 movies = pd.read_csv(r"""D:\Kaushik\PycharmProjects\untitled\data\u.item""", sep="|", header=None,encoding = "ISO-8859-1")
 movies.columns = ["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
-                      "Adventure", "Animation", "Childrens", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
-                      "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci_Fi", "Thriller", "War", "Western"]
+                      "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                      "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]
 
 genre = pd.read_csv(r"""D:\Kaushik\PycharmProjects\untitled\data\u.genre""", sep="|", header=None)
 genre.columns=["genre","value"]
@@ -20,6 +17,7 @@ user_data.columns = ["user_id", "movie_id", "rating", "ts"]
 user_combination_genre={}
 sorted_genre_matrix={}
 final_recommendation_genre_list=[]
+first_filter_movies = pd.DataFrame()
 
 # Creatng each movie rating accordignt to genre and finding the user's combinations in genre
 def create_genre_combination(df):
@@ -66,7 +64,7 @@ def top_genere_values(gener_values):
             sorted_genre_matrix[genre.iat[i,0]]=gener_values[i]
     # print (ls)
     # sorted_genre_matrix = sorted(ls.items(), key=operator.itemgetter(1))
-    print(sorted_genre_matrix)
+    # print(sorted_genre_matrix)
 
 # Creating Specific User above avg ratings data
 def user_ratings(user_id):
@@ -87,7 +85,6 @@ def find_accurate_recomendation_genre(combination_genres_sorted_list):
         if(items[i][1]>=avg):
             final_recommendation_genre_list.append(items[i][0])
 
-
 #Calculating recommended Genre Lists
 def calculate_recommended_genres():
     temp_dict=user_combination_genre
@@ -104,10 +101,42 @@ def calculate_recommended_genres():
 
 #input : nothing, output : movie dict , all movies with the sorted_genre_matrix
 def filter_movies_according_to_most_viewed_genre():
+    temp_m1 = pd.DataFrame(columns=["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
+                      "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                      "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
+    list_keys = list (sorted_genre_matrix.keys())
+    for i in range (0,len(list_keys)):
+        # print(list_keys[i])
+        temp = DataFrame(movies.loc[movies[list_keys[i]]==1])
+        frames = [temp_m1,temp]
+        temp_m1=pd.concat(frames)
+    temp_m1=temp_m1.drop_duplicates(subset=['movie_id'],keep=False)
+    global first_filter_movies
+    first_filter_movies=temp_m1
+    # print_full_df(first_filter_movies)
+    # print("Length of rows : %d" % (len(first_filter_movies)))
+
+def secondary_movie_filtering():
+    # # print(first_filter_movies)
+    # for i in range(0,len(final_recommendation_genre_list)):
+    #     temp = DataFrame(first_filter_movies)
+    #     flag=False
+    #     for j in range(0,len(final_recommendation_genre_list[i])):
+    #         print(final_recommendation_genre_list[i][j])
+    #         # print_full_df(temp)
+    #         temp = DataFrame(temp.loc[temp[final_recommendation_genre_list[i][j]]==1])
+    #         if(len(temp)==0):
+    #             flag=True
+    #             break
+    #     if(not flag or len(temp)!=0):
+    #         print_full_df(temp)
 
 
+    pass
 
-
+def print_full_df(x):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(x)
 
 def main():
     user = input("Enter User ID :")
@@ -116,6 +145,7 @@ def main():
     top_genere_values(gener_values)
     calculate_recommended_genres()
     filter_movies_according_to_most_viewed_genre()
+    secondary_movie_filtering()
 
 
 main()
